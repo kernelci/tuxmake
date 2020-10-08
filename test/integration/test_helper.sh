7e7f49f694f7ef101bed5583b6fb0c9dfcbd9a6e
Antonio Terceiro <antonio.terceiro@linaro.org>
2020-10-08 09:28:16 -0300

    integration tests: extract utility functions

--- test/integration/test_helper.sh
+++ test/integration/test_helper.sh
@@ -6,3 +6,34 @@ if ! command -v tuxmake >/dev/null; then
   export PYTHONPATH="${base}:${PYTHONPATH:+:}${PYTHONPATH:-}"
   export PATH="${base}/test/integration/bin:${PATH}"
 fi
+
+setUp() {
+  tmpdir=$(mktemp --directory --tmpdir tuxmake-integration-tests-XXXXXXXXXX)
+  export XDG_CACHE_HOME="${tmpdir}/cache"
+  cp -r $testdir/fakelinux/ "${tmpdir}/linux"
+  cd "${tmpdir}/linux"
+}
+
+tearDown() {
+  cd - >/dev/null
+  rm -rf "${tmpdir}"
+}
+
+run() {
+  if [ "${TMV:-}" = 1 ]; then
+    echo '    $' "$@"
+  fi
+  rc=0
+  "$@" > stdout 2> stderr || rc=$?
+  if [ "${TMV:-}" = 1 ]; then
+    cat stdout stderr | sed -e 's/^/    /'
+  fi
+  export rc
+}
+
+failrun() {
+  export FAIL="$1"
+  shift
+  run "$@"
+  unset FAIL
+}
