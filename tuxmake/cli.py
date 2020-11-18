kernelci/tuxmake

commit 940a8fa019aa446c4c5d19989a4c4e031f530f91

Rename to --docker-image/$TUXMAKE_DOCKER_IMAGE to --image/$TUXMAKE_IMAGE
Given that we have now more runtimes than docker that implement the
concept of images, it no longer makes sense to have a docker-specific
command line argument. Thus, we rename --docker-image to --image, and
the $TUXMAKE_DOCKER_IMAGE environment variable is now $TUXMAKE_IMAGE.

The old names are still accepted for backwards compatibility, but will
be removed in the future.

Additionally, --image will now only imply --runtime=docker if no runtime
is explicitly specified in the command line.

tuxmake/cli.py
@@ -81,13 +81,18 @@ def main(*argv):
         err = sys.stderr
 
     if options.docker_image:
-        options.runtime = "docker"
-        os.environ["TUXMAKE_DOCKER_IMAGE"] = options.docker_image
+        os.environ["TUXMAKE_IMAGE"] = options.docker_image
+        sys.stderr.write("W: --docker-image is deprecated; use --image instead\n")
+
+    if options.image:
+        if not options.runtime:
+            options.runtime = "docker"
+        os.environ["TUXMAKE_IMAGE"] = options.image
 
     build_args = {
         k: v
         for k, v in options.__dict__.items()
-        if v and k not in ["color", "docker_image", "shell"]
+        if v and k not in ["color", "image", "shell"]
     }
     try:
         result = build(**build_args, auto_cleanup=(not options.shell))

test/test_cli.py
@@ -99,7 +99,7 @@ class TestRuntime:
         assert args(builder).runtime == "docker"
 
 
-class TestDockerImage:
+class TestImage:
     @pytest.fixture(autouse=True)
     def environment(self, monkeypatch):
         env = {}
@@ -107,16 +107,24 @@ class TestDockerImage:
         return env
 
     def test_implies_runtime_docker(self, builder):
-        tuxmake("--docker-image=foobar")
+        tuxmake("--image=foobar")
         assert args(builder).runtime == "docker"
 
-    def test_not_passed_to_builder_class(self, builder):
-        tuxmake("--docker-image=foobar")
-        assert "docker_image" not in builder.call_args[1].keys()
+    def test_does_not_override_explicit_runtime(self, builder):
+        tuxmake("--image=foobar", "--runtime=podman")
+        assert args(builder).runtime == "podman"
 
-    def test_sets_TUXMAKE_DOCKER_IMAGE(self, environment):
+    def test_not_passed_to_builder_class(self, builder):
+        tuxmake("--image=foobar")
+        assert "image" not in builder.call_args[1].keys()
+
+    def test_sets_TUXMAKE_IMAGE(self, environment):
+        tuxmake("--image=foobar")
+        assert environment["TUXMAKE_IMAGE"] == "foobar"
+
+    def test_backwards_compat_with_docker_image(self, environment):
         tuxmake("--docker-image=foobar")
-        assert environment["TUXMAKE_DOCKER_IMAGE"] == "foobar"
+        assert environment["TUXMAKE_IMAGE"] == "foobar"
 
 
 class TestVerbosity:

test/test_cmdline.py
@@ -20,10 +20,10 @@ class TestCommandLine:
         cmd = cmdline.reproduce(Build(runtime="docker"))
         assert "--runtime=docker" in cmd
 
-    def test_docker_image(self, mocker, cmdline):
+    def test_image(self, mocker, cmdline):
         mocker.patch("tuxmake.runtime.DockerRuntime.get_image", return_value="myimage")
         cmd = cmdline.reproduce(Build(runtime="docker"))
-        assert "--docker-image=myimage" in cmd
+        assert "--image=myimage" in cmd
 
     def test_targets(self, cmdline):
         cmd = cmdline.reproduce(Build(targets=["config", "kernel"]))

test/test_runtime.py
@@ -47,23 +47,23 @@ class TestNullRuntime:
 
 
 @pytest.fixture
-def get_docker_image(mocker):
-    return mocker.patch("tuxmake.toolchain.Toolchain.get_docker_image")
+def get_image(mocker):
+    return mocker.patch("tuxmake.toolchain.Toolchain.get_image")
 
 
 class TestDockerRuntime:
-    def test_docker_image(self, build, get_docker_image):
-        get_docker_image.return_value = "foobarbaz"
+    def test_image(self, build, get_image):
+        get_image.return_value = "foobarbaz"
         runtime = DockerRuntime()
         assert runtime.get_image(build) == "foobarbaz"
 
-    def test_override_docker_image(self, build, monkeypatch):
-        monkeypatch.setenv("TUXMAKE_DOCKER_IMAGE", "foobar")
+    def test_override_image(self, build, monkeypatch):
+        monkeypatch.setenv("TUXMAKE_IMAGE", "foobar")
         runtime = DockerRuntime()
         assert runtime.get_image(build) == "foobar"
 
-    def test_prepare(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "myimage"
+    def test_prepare(self, build, get_image, mocker):
+        get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
         DockerRuntime().prepare(build)
         check_call.assert_called_with(["docker", "pull", "myimage"])
@@ -130,8 +130,8 @@ class TestDockerRuntime:
 
 
 class TestDockerLocalRuntime:
-    def test_prepare_checks_local_image(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "mylocalimage"
+    def test_prepare_checks_local_image(self, build, get_image, mocker):
+        get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")
         runtime = DockerLocalRuntime()
 
@@ -142,8 +142,8 @@ class TestDockerLocalRuntime:
             stderr=subprocess.DEVNULL,
         )
 
-    def test_prepare_image_not_found(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "foobar"
+    def test_prepare_image_not_found(self, build, get_image, mocker):
+        get_image.return_value = "foobar"
         mocker.patch(
             "subprocess.check_call",
             side_effect=subprocess.CalledProcessError(
@@ -162,8 +162,8 @@ class TestDockerLocalRuntime:
 
 
 class TestPodmanRuntime:
-    def test_prepare(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "myimage"
+    def test_prepare(self, build, get_image, mocker):
+        get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
         PodmanRuntime().prepare(build)
         check_call.assert_called_with(["podman", "pull", "myimage"])
@@ -185,8 +185,8 @@ class TestPodmanRuntime:
 
 
 class TestPodmanLocalRuntime:
-    def test_prepare_checks_local_image(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "mylocalimage"
+    def test_prepare_checks_local_image(self, build, get_image, mocker):
+        get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")
         runtime = PodmanLocalRuntime()
 
@@ -197,8 +197,8 @@ class TestPodmanLocalRuntime:
             stderr=subprocess.DEVNULL,
         )
 
-    def test_prepare_image_not_found(self, build, get_docker_image, mocker):
-        get_docker_image.return_value = "foobar"
+    def test_prepare_image_not_found(self, build, get_image, mocker):
+        get_image.return_value = "foobar"
         mocker.patch(
             "subprocess.check_call",
             side_effect=subprocess.CalledProcessError(

test/test_toolchain.py
@@ -17,8 +17,8 @@ def arm64():
 
 
 class TestGcc:
-    def test_docker_image(self, gcc, arm64):
-        assert gcc.get_docker_image(arm64) == "tuxmake/arm64_gcc"
+    def test_image(self, gcc, arm64):
+        assert gcc.get_image(arm64) == "tuxmake/arm64_gcc"
 
 
 @pytest.fixture
@@ -27,8 +27,8 @@ def clang():
 
 
 class TestClang:
-    def test_docker_image(self, clang, arm64):
-        assert clang.get_docker_image(arm64) == "tuxmake/arm64_clang"
+    def test_image(self, clang, arm64):
+        assert clang.get_image(arm64) == "tuxmake/arm64_clang"
 
 
 def test_compiler_name(gcc, arm64):
@@ -40,13 +40,13 @@ def test_compiler_name(gcc, arm64):
 
 
 class TestLLVM:
-    def test_docker_image_unversioned(self, arm64):
+    def test_image_unversioned(self, arm64):
         llvm = Toolchain("llvm")
-        assert llvm.get_docker_image(arm64) == "tuxmake/arm64_clang"
+        assert llvm.get_image(arm64) == "tuxmake/arm64_clang"
 
-    def test_docker_image_versioned(self, arm64):
+    def test_image_versioned(self, arm64):
         llvm = Toolchain("llvm-10")
-        assert llvm.get_docker_image(arm64) == "tuxmake/arm64_clang-10"
+        assert llvm.get_image(arm64) == "tuxmake/arm64_clang-10"
 
     def test_compiler_unversioned(self, arm64):
         assert Toolchain("llvm").compiler(arm64) == "clang"

tuxmake/cmdline.py
@@ -111,9 +111,10 @@ def build_parser(cls=argparse.ArgumentParser, **kwargs):
     )
     buildenv.add_argument(
         "-i",
-        "--docker-image",
-        help="Docker image to build with (implies --docker). {toolchain} and {arch} get replaced by the names of the toolchain and architecture selected for the build. Implies --runtime=docker. (default: tuxmake-provided images).",
+        "--image",
+        help="Image to build with, for container-based runtimes (docker, podman etc). {toolchain} and {arch} get replaced by the names of the toolchain and architecture selected for the build. Implies --runtime=docker if no runtime is explicit specified. (default: tuxmake-provided images).",
     )
+    buildenv.add_argument("--docker-image", help="Alias for --image (deprecated).")
     buildenv.add_argument(
         "-v",
         "--verbose",
@@ -246,7 +247,7 @@ class CommandLine:
                     cmd.append(c)
         image = build.runtime.get_image(build)
         if image:
-            cmd.append(f"--docker-image={image}")
+            cmd.append(f"--image={image}")
         for target in build.targets:
             cmd.append(target.name)
 

tuxmake/runtime.py
@@ -124,7 +124,7 @@ class DockerRuntime(Runtime):
         }
 
     def is_supported(self, arch, toolchain):
-        image_name = toolchain.get_docker_image(arch)
+        image_name = toolchain.get_image(arch)
         image = self.toolchain_images_map.get(image_name)
         if image:
             return host_arch.name in image.hosts or any(
@@ -134,8 +134,10 @@ class DockerRuntime(Runtime):
             return False
 
     def get_image(self, build):
-        return os.getenv("TUXMAKE_DOCKER_IMAGE") or build.toolchain.get_docker_image(
-            build.target_arch
+        return (
+            os.getenv("TUXMAKE_IMAGE")
+            or os.getenv("TUXMAKE_DOCKER_IMAGE")
+            or build.toolchain.get_image(build.target_arch)
         )
 
     def prepare(self, build):

tuxmake/toolchain.py
@@ -18,7 +18,7 @@ class Toolchain(ConfigurableObject):
 
     def __init_config__(self):
         self.makevars = self.config["makevars"]
-        self.docker_image = self.config["docker"]["image"]
+        self.image = self.config["docker"]["image"]
         self.__compiler__ = self.config["metadata"]["compiler"]
 
     def expand_makevars(self, arch):
@@ -28,8 +28,8 @@ class Toolchain(ConfigurableObject):
             for k, v in self.makevars.items()
         }
 
-    def get_docker_image(self, arch):
-        return self.docker_image.format(
+    def get_image(self, arch):
+        return self.image.format(
             toolchain=self.name, arch=arch.name, version_suffix=self.version_suffix
         )
 

commit 83ff496c4bc2aaaae7d52b2765c133135a1a2206

cli: fix --docker-image
This was a regression introduced in
940a8fa019aa446c4c5d19989a4c4e031f530f91 when --image was added and
--docker-image was marked as deprecated.

Closes #91

tuxmake/cli.py
@@ -92,7 +92,7 @@ def main(*argv):
     build_args = {
         k: v
         for k, v in options.__dict__.items()
-        if v and k not in ["color", "image", "shell"]
+        if v and k not in ["color", "docker_image", "image", "shell"]
     }
     try:
         result = build(**build_args, auto_cleanup=(not options.shell))

test/test_cli.py
@@ -122,9 +122,10 @@ class TestImage:
         tuxmake("--image=foobar")
         assert environment["TUXMAKE_IMAGE"] == "foobar"
 
-    def test_backwards_compat_with_docker_image(self, environment):
+    def test_backwards_compat_with_docker_image(self, environment, builder):
         tuxmake("--docker-image=foobar")
         assert environment["TUXMAKE_IMAGE"] == "foobar"
+        assert "docker_image" not in builder.call_args[1].keys()
 
 
 class TestVerbosity: