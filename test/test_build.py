kernelci/tuxmake

commit 96738e76ace166ef62ab8bfa696fe1d8eab01f02

runtime: make runtime reusable outside of TuxMake

tuxmake/build.py
@@ -3,10 +3,10 @@ from collections import OrderedDict
 from pathlib import Path
 import json
 import os
+import re
 import signal
 import shutil
 import subprocess
-import sys
 import time
 from tuxmake import __version__
 from tuxmake import deprecated
@@ -17,6 +17,7 @@ from tuxmake.wrapper import Wrapper
 from tuxmake.output import get_new_output_dir
 from tuxmake.target import create_target
 from tuxmake.runtime import Runtime
+from tuxmake.runtime import Terminated
 from tuxmake.metadata import MetadataCollector
 from tuxmake.exceptions import EnvironmentCheckFailed
 from tuxmake.exceptions import UnrecognizedSourceTree
@@ -78,12 +79,6 @@ class BuildInfo:
         `True` if this target was skipped.
         """
         return self.status == "SKIP"
-
-
-class Terminated(Exception):
-    @staticmethod
-    def handle_signal(signum, _):
-        raise Terminated(f"received signal {signum}; terminating ...")
 
 
 class Build:
@@ -244,7 +239,6 @@ class Build:
         self.offline = False
 
         self.artifacts = {"log": ["build.log", "build-debug.log"]}
-        self.__logger__ = None
         self.__status__ = {}
         self.__durations__ = {}
         self.metadata_collector = MetadataCollector(self)
@@ -297,15 +291,31 @@ class Build:
     def prepare(self):
         self.wrapper.prepare_host()
 
+        self.runtime.basename = "build"
+        self.runtime.quiet = self.quiet
+        self.runtime.source_dir = self.source_tree
+        self.runtime.output_dir = self.output_dir
+        self.runtime.add_volume(self.build_dir)
+        if self.wrapper.path:
+            self.runtime.add_volume(
+                str(self.wrapper.path), f"/usr/local/bin/{self.wrapper.name}"
+            )
+        env = dict(**self.wrapper.environment, **self.environment, LANG="C")
+        self.runtime.environment = env
+        for k, v in env.items():
+            if k.endswith("_DIR"):
+                path = "/" + re.sub(r"[^a-zA-Z0-9]+", "-", k.lower())
+                self.runtime.add_volume(v, path)
+                v = path
+        self.runtime.prepare()
+        self.wrapper.prepare_runtime(self)
+
         if self.toolchain.version_suffix and self.runtime.name == "null":
             toolchain = self.toolchain
             compiler = toolchain.compiler(self.target_arch)
             self.log(
                 f"W: Requested {toolchain}, but versioned toolchains are not supported by the null runtime. Will use whatever version of {compiler} that you have installed. To ensure {toolchain} is used, try use a container-based runtime instead."
             )
-
-        self.runtime.prepare(self)
-        self.wrapper.prepare_runtime(self)
 
     @property
     def output_dir(self):
@@ -376,43 +386,17 @@ class Build:
         else:
             expect_failure = False
 
-        final_cmd = self.runtime.get_command_line(
-            cmd, interactive, offline=self.offline
-        )
-        extra_env = dict(**self.wrapper.environment, **self.environment, LANG="C")
-        env = dict(os.environ, **extra_env)
-
-        logger = self.logger.stdin
-        if interactive:
-            stdout = stderr = stdin = None
-        else:
-            stdin = subprocess.DEVNULL
-            stderr = logger
-            if not stdout:
-                self.log(quote_command_line(cmd))
-                stdout = logger
-
-        debug(f"Command: {final_cmd}")
-        if extra_env:
-            debug(f"Environment: {extra_env}")
         try:
             with self.measure_duration("Command"):
-                process = subprocess.Popen(
-                    final_cmd,
-                    cwd=self.source_tree,
-                    env=env,
-                    stdin=stdin,
+                return self.runtime.run_cmd(
+                    cmd,
+                    interactive=interactive,
                     stdout=stdout,
-                    stderr=stderr,
+                    expect_failure=expect_failure,
+                    offline=self.offline,
                 )
-            process.communicate()
-            if expect_failure:
-                return process.returncode != 0
-            else:
-                return process.returncode == 0
         except (KeyboardInterrupt, Terminated) as ex:
             self.log(str(ex))
-            process.terminate()
             self.interrupted = True
             return False
 
@@ -460,26 +444,8 @@ class Build:
             **self.target_overrides,
         )
 
-    @property
-    def logger(self):
-        if not self.__logger__:
-            if self.quiet:
-                stdout = subprocess.DEVNULL
-            else:
-                stdout = sys.stdout
-            self.__logger__ = subprocess.Popen(
-                [
-                    str(Runtime.bindir / "tuxmake-logger"),
-                    str(self.output_dir / "build.log"),
-                    str(self.output_dir / "build-debug.log"),
-                ],
-                stdin=subprocess.PIPE,
-                stdout=stdout,
-            )
-        return self.__logger__
-
     def log(self, *stuff):
-        subprocess.call(["echo"] + list(stuff), stdout=self.logger.stdin)
+        self.runtime.log(*stuff)
 
     def make_args(self, makevars):
         # we want to override target makevars with user provided make_variables
@@ -601,7 +567,7 @@ class Build:
             "duration": self.__durations__,
         }
         self.metadata["tuxmake"] = {"version": __version__}
-        self.metadata["runtime"] = self.runtime.get_metadata(self)
+        self.metadata["runtime"] = self.runtime.get_metadata()
 
         extracted = self.metadata_collector.collect()
         self.metadata.update(extracted)
@@ -616,17 +582,13 @@ class Build:
         parser.parse(self.output_dir / "build.log")
         return parser.errors, parser.warnings
 
-    def terminate(self):
-        self.logger.communicate()
-        self.logger.terminate()
-
     def cleanup(self):
         self.runtime.cleanup()
         if self.clean_build_tree:
             shutil.rmtree(self.build_dir, ignore_errors=True)
 
     def check_environment(self):
-        self.runtime.prepare(self)
+        self.runtime.prepare()
         cmd = [str(self.runtime.get_check_environment_command())]
         cmd.append(f"{self.target_arch.name}_{self.toolchain.name}")
         cross = self.makevars.get("CROSS_COMPILE")
@@ -670,7 +632,6 @@ class Build:
                     self.collect_metadata()
 
             with self.measure_duration("Cleanup", metadata="cleanup"):
-                self.terminate()
                 if self.auto_cleanup:
                     self.cleanup()
 

test/test_build.py
@@ -308,7 +308,6 @@ def test_quiet(linux, capfd):
 class TestInterruptedBuild:
     @pytest.fixture
     def interrupted(self, mocker, Popen):
-        mocker.patch("tuxmake.build.Build.logger")
         process = mocker.MagicMock()
         Popen.return_value = process
         process.communicate.side_effect = KeyboardInterrupt()
@@ -638,9 +637,7 @@ class TestRuntime:
         runtime.get_command_line.return_value = ["true"]
         build = Build(tree=linux, runtime="docker")
         build.run_cmd(["true"], interactive=True)
-        runtime.get_command_line.assert_called_with(
-            ["true"], True, offline=build.offline
-        )
+        assert kwargs(runtime.run_cmd)["interactive"]
 
 
 class TestEnvironment:
@@ -650,8 +647,8 @@ class TestEnvironment:
             environment={"KCONFIG_ALLCONFIG": "foo.config"},
             targets=["config"],
         )
-        b.build(b.targets[0])
-        assert kwargs(Popen)["env"]["KCONFIG_ALLCONFIG"] == "foo.config"
+        b.prepare()
+        assert b.runtime.environment["KCONFIG_ALLCONFIG"] == "foo.config"
 
 
 class TestMakeVariables:
@@ -668,10 +665,11 @@ class TestMakeVariables:
 class TestCompilerWrappers:
     def test_ccache(self, linux, Popen):
         b = Build(tree=linux, targets=["config"], wrapper="ccache")
-        b.build(b.targets[0])
-        assert "CC=ccache gcc" in args(Popen)
-        assert "HOSTCC=ccache gcc" in args(Popen)
-        assert "CCACHE_DIR" in kwargs(Popen)["env"]
+        b.prepare()
+        assert "CCACHE_DIR" in b.runtime.environment
+        makevars = b.makevars
+        assert makevars["CC"] == "ccache gcc"
+        assert makevars["HOSTCC"] == "ccache gcc"
 
     def test_ccache_gcc_v(self, linux, Popen):
         b = Build(tree=linux, targets=["config"], toolchain="gcc-10", wrapper="ccache")
@@ -705,6 +703,13 @@ class TestCompilerWrappers:
         )
         b.build(b.targets[0])
         assert "CC=ccache clang" in args(Popen)
+
+    def test_sccache_with_path(self, linux, mocker, Popen):
+        add_volume = mocker.patch("tuxmake.runtime.Runtime.add_volume")
+        b = Build(tree=linux, wrapper="/path/to/sccache")
+        b.prepare()
+        volumes = [call[0] for call in add_volume.call_args_list]
+        assert ("/path/to/sccache", "/usr/local/bin/sccache") in volumes
 
 
 @pytest.mark.skipif(
@@ -790,7 +795,9 @@ class TestDebug:
 
     @pytest.fixture
     def debug_build(self, linux):
-        return Build(tree=linux, debug=True, environment={"FOO": "BAR"})
+        b = Build(tree=linux, debug=True, environment={"FOO": "BAR"})
+        b.prepare()
+        return b
 
     @pytest.fixture
     def err(self, debug_build, mocker, capfd):
@@ -821,7 +828,7 @@ class TestPrepare:
         )
         mocker.patch(
             "tuxmake.runtime.NullRuntime.prepare",
-            side_effect=lambda _: order.append("runtime"),
+            side_effect=lambda: order.append("runtime"),
         )
         mocker.patch(
             "tuxmake.wrapper.Wrapper.prepare_runtime",

mkdocs.yml
@@ -38,7 +38,9 @@ nav:
   - Custom Containers: custom_containers.md
 - Reference:
   - Command Line Reference: cli.md
-  - Python API Reference: python.md
+  - Python API Reference:
+    - Build: python.md
+    - Runtime Library: python_runtime.md
 - Community:
   - Chat: chat.md
   - Code of Conduct: code-of-conduct.md

test/integration/bin/tuxcmd
@@ -1,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+from tuxmake.runtime import Runtime
+
+def main(argv=sys.argv):
+	parser = argparse.ArgumentParser(
+		prog="tuxdate",
+		usage="%(prog)s [OPTIONS] CMD ...",
+		description="Runs CMD, maybe using containerss (example tuxmake runtime library client)",
+		add_help=True,
+	)
+
+	parser.add_argument("-r", '--runtime', type=str, default="podman", help="Container runtime to use")
+	parser.add_argument("-i", '--image', type=str, default="debian", help="Container image to use")
+	parser.add_argument("-I", '--interactive', action="store_true", help="Interactive (allocate a TTY)")
+	parser.add_argument("command", nargs="*")
+	options = parser.parse_args(argv[1:])
+
+	runtime = Runtime.get(options.runtime)
+	runtime.set_image(options.image)
+	runtime.prepare()
+	runtime.run_cmd(options.command, interactive=options.interactive, offline=False)
+	runtime.cleanup()
+
+
+
+main()

test/test_runtime.py
@@ -11,13 +11,19 @@ from tuxmake.runtime import DockerRuntime
 from tuxmake.runtime import DockerLocalRuntime
 from tuxmake.runtime import PodmanRuntime
 from tuxmake.runtime import PodmanLocalRuntime
-from tuxmake.wrapper import Wrapper
+from tuxmake.runtime import Terminated
 
 
 @pytest.fixture
 def build(linux):
     b = Build(linux)
     return b
+
+
+class TestTerminated:
+    def test_basics(self):
+        with pytest.raises(Terminated):
+            Terminated.handle_signal(9, "BOOM")
 
 
 class TestGetRuntime:
@@ -42,6 +48,20 @@ class TestRuntime:
         monkeypatch.setattr(Runtime, "name", "invalid")
         with pytest.raises(InvalidRuntimeError):
             Runtime()
+
+    def test_run_cmd_interactive(self, Popen, mocker):
+        get_command_line = mocker.patch(
+            "tuxmake.runtime.Runtime.get_command_line", return_value=["/bin/bash"]
+        )
+        runtime = NullRuntime()
+        runtime.run_cmd(["/bin/bash"], interactive=True)
+        kwargs = Popen.call_args[1]
+        get_command_line.assert_called_with(
+            ["/bin/bash"], interactive=True, offline=True
+        )
+        assert kwargs["stdin"] is None
+        assert kwargs["stdout"] is None
+        assert kwargs["stderr"] is None
 
 
 class TestNullRuntime:
@@ -94,23 +114,23 @@ class TestGetImage:
 
 
 class TestDockerRuntime(TestContainerRuntime):
-    def test_get_metadata(self, build, get_image, mocker):
+    def test_get_metadata(self, get_image, mocker):
         get_image.return_value = "tuxmake/theimage"
         mocker.patch(
             "subprocess.check_output",
             return_value=b"tuxmake/theimage@sha256:deadbeef",
         )
-        metadata = DockerRuntime().get_metadata(build)
+        metadata = DockerRuntime().get_metadata()
         assert metadata["image_name"] == "tuxmake/theimage"
         assert metadata["image_digest"] == "tuxmake/theimage@sha256:deadbeef"
 
-    def test_prepare(self, build, get_image, mocker):
+    def test_prepare(self, get_image, mocker):
         get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
-        DockerRuntime().prepare(build)
+        DockerRuntime().prepare()
         check_call.assert_called_with(["docker", "pull", "myimage"])
 
-    def test_prepare_pull_only_once_a_day(self, build, get_image, mocker):
+    def test_prepare_pull_only_once_a_day(self, get_image, mocker):
         get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
         now = 1614000983
@@ -122,73 +142,80 @@ class TestDockerRuntime(TestContainerRuntime):
         )
 
         # first call
-        PodmanRuntime().prepare(build)
+        PodmanRuntime().prepare()
         assert len(check_call.call_args_list) == 1
 
         # after 2 hours, no need to pull
-        PodmanRuntime().prepare(build)
+        PodmanRuntime().prepare()
         assert len(check_call.call_args_list) == 1
 
         # after 2 days, pull again
-        PodmanRuntime().prepare(build)
+        PodmanRuntime().prepare()
         assert len(check_call.call_args_list) == 2
 
-    def test_start_container(self, build, container_id):
+    def test_start_container(self, container_id):
         runtime = DockerRuntime()
-        runtime.start_container(build)
+        runtime.start_container()
         assert runtime.container_id == container_id
 
-    def test_cleanup(self, build, container_id, mocker):
+    def test_cleanup(self, container_id, mocker):
         check_call = mocker.patch("subprocess.check_call")
         runtime = DockerRuntime()
-        runtime.start_container(build)
+        runtime.start_container()
         runtime.cleanup()
         cmd = check_call.call_args[0][0]
         assert cmd[0:2] == ["docker", "stop"]
         assert cmd[-1] == container_id
 
-    def test_cleanup_before_container_exists(self, build):
+    def test_cleanup_before_container_exists(self):
         runtime = DockerRuntime()
         assert runtime.container_id is None
         runtime.cleanup()  # if this doesn't crash we are good
 
-    def test_get_command_line(self, build):
+    def test_get_command_line(self):
         cmd = DockerRuntime().get_command_line(["date"], False)
         assert cmd[0:2] == ["docker", "exec"]
         assert cmd[-1] == "date"
 
     def test_environment(self, linux, spawn_container):
-        build = Build(linux, environment={"FOO": "BAR"})
-        DockerRuntime().start_container(build)
+        runtime = DockerRuntime()
+        runtime.environment["FOO"] = "BAR"
+        runtime.start_container()
         cmd = spawn_container.call_args[0][0]
         assert "--env=FOO=BAR" in cmd
 
-    def test_ccache(self, build, home, spawn_container):
-        ccache = Wrapper("ccache")
-        orig_ccache_dir = ccache.environment["CCACHE_DIR"]
-        build.wrapper = ccache
-        DockerRuntime().start_container(build)
+    def test_output_dir(self, linux, tmp_path, spawn_container):
+        runtime = DockerRuntime()
+        runtime.output_dir = tmp_path
+        runtime.start_container()
         cmd = spawn_container.call_args[0][0]
-        assert "--env=CCACHE_DIR=/ccache-dir" in cmd
-        assert f"--volume={orig_ccache_dir}:/ccache-dir" in cmd
+        assert f"--volume={tmp_path}:{tmp_path}" in cmd
 
-    def test_sccache_with_path(self, build, home, spawn_container):
-        sccache_from_host = Wrapper("/opt/bin/sccache")
-        build.wrapper = sccache_from_host
-        DockerRuntime().start_container(build)
+    def test_source_dir(self, linux, tmp_path, spawn_container):
+        runtime = DockerRuntime()
+        runtime.source_dir = tmp_path
+        runtime.start_container()
         cmd = spawn_container.call_args[0][0]
-        assert "--volume=/opt/bin/sccache:/usr/local/bin/sccache" in cmd
+        assert f"--volume={tmp_path}:{tmp_path}" in cmd
 
-    def test_TUXMAKE_DOCKER_RUN(self, build, monkeypatch, spawn_container):
+    def test_volume(self, linux, spawn_container):
+        runtime = DockerRuntime()
+        path = "/path/to/something"
+        runtime.add_volume(path)
+        runtime.start_container()
+        cmd = spawn_container.call_args[0][0]
+        assert f"--volume={path}:{path}" in cmd
+
+    def test_TUXMAKE_DOCKER_RUN(self, monkeypatch, spawn_container):
         monkeypatch.setenv(
             "TUXMAKE_DOCKER_RUN", "--hostname=foobar --env=FOO='bar baz'"
         )
-        DockerRuntime().start_container(build)
+        DockerRuntime().start_container()
         cmd = spawn_container.call_args[0][0]
         assert "--hostname=foobar" in cmd
         assert "--env=FOO=bar baz" in cmd
 
-    def test_interactive(self, build):
+    def test_interactive(self):
         cmd = DockerRuntime().get_command_line(["bash"], True)
         assert "--interactive" in cmd
         assert "--tty" in cmd
@@ -219,12 +246,12 @@ class TestDockerRuntime(TestContainerRuntime):
 
 
 class TestDockerRuntimeSpawnContainer(FakeGetImage):
-    def test_spawn_container(self, build, mocker, container_id):
+    def test_spawn_container(self, mocker, container_id):
         check_output = mocker.patch(
             "subprocess.check_output", return_value=container_id.encode("utf-8")
         )
         runtime = DockerRuntime()
-        runtime.start_container(build)
+        runtime.start_container()
         cmd = check_output.call_args[0][0]
         assert cmd[0:2] == ["docker", "run"]
         assert runtime.container_id == container_id
@@ -232,13 +259,13 @@ class TestDockerRuntimeSpawnContainer(FakeGetImage):
 
 class TestDockerRuntimeOfflineAvailable(FakeGetImage):
     @pytest.fixture
-    def runtime(self, build, container_id, mocker):
+    def runtime(self, container_id, mocker):
         mocker.patch(
             "tuxmake.runtime.DockerRuntime.spawn_container", return_value=container_id
         )
         mocker.patch("tuxmake.runtime.ContainerRuntime.prepare_image")
         r = DockerRuntime()
-        r.prepare(build)
+        r.prepare()
         return r
 
     def test_offline_available(self, runtime, mocker):
@@ -258,19 +285,19 @@ class TestDockerRuntimeOfflineAvailable(FakeGetImage):
 
 
 class TestDockerLocalRuntime(TestContainerRuntime):
-    def test_prepare_checks_local_image(self, build, get_image, mocker):
+    def test_prepare_checks_local_image(self, get_image, mocker):
         get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")
         runtime = DockerLocalRuntime()
 
-        runtime.prepare(build)
+        runtime.prepare()
         check_call.assert_called_with(
             ["docker", "image", "inspect", "mylocalimage"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
 
-    def test_prepare_image_not_found(self, build, get_image, mocker):
+    def test_prepare_image_not_found(self, get_image, mocker):
         get_image.return_value = "foobar"
         mocker.patch(
             "subprocess.check_call",
@@ -279,7 +306,7 @@ class TestDockerLocalRuntime(TestContainerRuntime):
             ),
         )
         with pytest.raises(RuntimePreparationFailed) as exc:
-            DockerLocalRuntime().prepare(build)
+            DockerLocalRuntime().prepare()
         assert "image foobar not found locally" in str(exc)
 
     def test_listed_as_supported(self):
@@ -290,13 +317,13 @@ class TestDockerLocalRuntime(TestContainerRuntime):
 
 
 class TestPodmanRuntime(TestContainerRuntime):
-    def test_prepare(self, build, get_image, mocker):
+    def test_prepare(self, get_image, mocker):
         get_image.return_value = "myimage"
         check_call = mocker.patch("subprocess.check_call")
-        PodmanRuntime().prepare(build)
+        PodmanRuntime().prepare()
         check_call.assert_called_with(["podman", "pull", "myimage"])
 
-    def test_get_command_line(self, build):
+    def test_get_command_line(self):
         cmd = PodmanRuntime().get_command_line(["date"], False)
         assert cmd[0:2] == ["podman", "exec"]
         assert cmd[-1] == "date"
@@ -304,49 +331,49 @@ class TestPodmanRuntime(TestContainerRuntime):
     def test_listed_as_supported(self):
         assert "podman" in Runtime.supported()
 
-    def test_no_user_option(self, build, get_image, spawn_container):
-        PodmanRuntime().start_container(build)
+    def test_no_user_option(self, get_image, spawn_container):
+        PodmanRuntime().start_container()
         cmd = spawn_container.call_args[0][0]
         assert len([c for c in cmd if "--user=" in c]) == 0
 
     def test_str(self):
         assert str(PodmanRuntime()) == "podman"
 
-    def test_TUXMAKE_PODMAN_RUN(self, build, monkeypatch, spawn_container):
+    def test_TUXMAKE_PODMAN_RUN(self, monkeypatch, spawn_container):
         monkeypatch.setenv(
             "TUXMAKE_PODMAN_RUN", "--hostname=foobar --env=FOO='bar baz'"
         )
-        PodmanRuntime().start_container(build)
+        PodmanRuntime().start_container()
         cmd = spawn_container.call_args[0][0]
         assert "--hostname=foobar" in cmd
         assert "--env=FOO=bar baz" in cmd
 
-    def test_selinux_label(self, build, get_image, spawn_container):
-        PodmanRuntime().start_container(build)
+    def test_selinux_label(self, get_image, spawn_container):
+        PodmanRuntime().start_container()
         cmd = spawn_container.call_args[0][0]
         volumes = [o for o in cmd if o.startswith("--volume=")]
         assert all([v.endswith(":z") for v in volumes])
 
-    def test_logging_level(self, build, spawn_container):
-        PodmanRuntime().start_container(build)
+    def test_logging_level(self, spawn_container):
+        PodmanRuntime().start_container()
         cmd = spawn_container.call_args[0][0]
         assert "--log-level=ERROR" in cmd
 
 
 class TestPodmanLocalRuntime(TestContainerRuntime):
-    def test_prepare_checks_local_image(self, build, get_image, mocker):
+    def test_prepare_checks_local_image(self, get_image, mocker):
         get_image.return_value = "mylocalimage"
         check_call = mocker.patch("subprocess.check_call")
         runtime = PodmanLocalRuntime()
 
-        runtime.prepare(build)
+        runtime.prepare()
         check_call.assert_called_with(
             ["podman", "image", "inspect", "mylocalimage"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
 
-    def test_prepare_image_not_found(self, build, get_image, mocker):
+    def test_prepare_image_not_found(self, get_image, mocker):
         get_image.return_value = "foobar"
         mocker.patch(
             "subprocess.check_call",
@@ -355,7 +382,7 @@ class TestPodmanLocalRuntime(TestContainerRuntime):
             ),
         )
         with pytest.raises(RuntimePreparationFailed) as exc:
-            PodmanLocalRuntime().prepare(build)
+            PodmanLocalRuntime().prepare()
         assert "image foobar not found locally" in str(exc)
 
     def test_listed_as_supported(self):

commit b9f146c28909cfa890f18f7526f207abeafb2eb4

build: fix ccache usage with container runtimes
The transformation in CCACHE_DIR to a bind mounted directory that was
different from the original was previously encapsulated in the
container runtime. But when it was moved to the Build class, the
CCACHE_DIR mapping was done *after* the environment was set, so
CCACHE_DIR would point to the wrong location during the build.

In order to also support the null runtime, we need to just use the
original value of CCACHE_DIR.

Fixes: 96738e76ace1 ("runtime: make runtime reusable outside of TuxMake")

tuxmake/build.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 from pathlib import Path
 import json
 import os
-import re
 import signal
 import shutil
 import subprocess
@@ -300,13 +299,12 @@ class Build:
             self.runtime.add_volume(
                 str(self.wrapper.path), f"/usr/local/bin/{self.wrapper.name}"
             )
-        env = dict(**self.wrapper.environment, **self.environment, LANG="C")
+        wenv = self.wrapper.environment
+        env = dict(**wenv, **self.environment, LANG="C")
         self.runtime.environment = env
-        for k, v in env.items():
+        for k, v in wenv.items():
             if k.endswith("_DIR"):
-                path = "/" + re.sub(r"[^a-zA-Z0-9]+", "-", k.lower())
-                self.runtime.add_volume(v, path)
-                v = path
+                self.runtime.add_volume(v)
         self.runtime.prepare()
         self.wrapper.prepare_runtime(self)
 

test/test_build.py
@@ -693,6 +693,14 @@ class TestCompilerWrappers:
         assert makevars["CC"] == "ccache gcc"
         assert makevars["HOSTCC"] == "ccache gcc"
 
+    def test_ccache_with_container_runtime(self, linux, Popen, mocker):
+        b = Build(tree=linux, targets=["config"], wrapper="ccache", runtime="podman")
+        mocker.patch("tuxmake.runtime.ContainerRuntime.prepare")
+        mocker.patch("tuxmake.wrapper.Wrapper.prepare_runtime")
+        b.prepare()
+        cache_dir = b.wrapper.environment["CCACHE_DIR"]
+        assert (cache_dir, cache_dir) in b.runtime.volumes
+
     def test_ccache_gcc_v(self, linux, Popen):
         b = Build(tree=linux, targets=["config"], toolchain="gcc-10", wrapper="ccache")
         b.build(b.targets[0])